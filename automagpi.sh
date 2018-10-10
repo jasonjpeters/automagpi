eb7322ff6025407447332a4cbcc20b607337c5b8
JasonJPeters <jasonjpeters@gmail.com>
2018-10-09 22:56:16 -0700

    init

---
+++ automagpi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MAGPI_URL="https://www.raspberrypi.org/magpi-issues/"
+
+curl ${MAGPI_URL} | grep -o 'href="[^"]*.[pdf]"' | cut -d\" -f2 | uniq > ./library.list
+
+while read name; do
+    if [ ! -f ./library/${name} ]; then
+        wget -e robots=off -O ./library/${name} ${MAGPI_URL}/${name}
+    fi
+done < ./library.list
