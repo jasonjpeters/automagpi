aaa42276047668963c84dbaf59a2a2ed38cad70e
JasonJPeters <jasonjpeters@gmail.com>
2018-10-09 23:08:54 -0700

    updated library file to hidden

--- automagpi.sh
+++ automagpi.sh
@@ -2,10 +2,12 @@
 
 MAGPI_URL="https://www.raspberrypi.org/magpi-issues/"
 
-curl ${MAGPI_URL} | grep -o 'href="[^"]*.[pdf]"' | cut -d\" -f2 | uniq > ./library.list
+
+curl ${MAGPI_URL} | grep -o 'href="[^"]*.[pdf]"' | cut -d\" -f2 | uniq > ./.library.list
+
 
 while read name; do
     if [ ! -f ./library/${name} ]; then
-        wget -e robots=off -O ./library/${name} ${MAGPI_URL}/${name}
+        # wget -e robots=off -O ./library/${name} ${MAGPI_URL}/${name}
     fi
-done < ./library.list
+done < ./.library.list
